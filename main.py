2a9f818b000c815dcee1b0d3092acde7c02d7bf3
Maxzett <maxi.marzetti03@gmail.com>
2024-11-14 11:47:59 -0300

    entrega rpp

---
+++ main.py
@@ -0,0 +1,58 @@
+from paquete.modulo import *
+
+depositos = ['Tierra del Fuego', 'Tucuman', 'Mendoza', 'BsAs', 'Misiones', 'Santa Fe']
+equipos = ['Barcelona', 'Inter Miami', 'PSG', 'Manchester City', 'Real Madrid']
+
+#matriz prueba
+matriz_prueba = [[3,4,4,25,60],
+                 [17,50,10,8,9],
+                 [25,46,81,4,6],
+                 [56,7,43,49,12],
+                 [13,58,32,26,28],
+                 [47,9,14,41,8]]
+
+
+print('Seleccione una opcion:')
+print('1- carga secuencial / obtener existencias')
+print('2- Mostrar depositos que tienen en stock mas de 10.000 camisetas.')
+print('3- Mostrar equipos que hay en stock más de 5.000 camisetas.')
+print('4- Obtener máxima cantidad de camisetas de cada equipo. Mostrar en qué depósito se encuentra')
+print('5- Cargar ventas')
+print()
+opcion = input('Ingrese una opcion: ')
+
+
+match opcion:
+    
+    case '1':
+        pass
+
+    case '2':
+        totales_por_deposito = calcular_totales(matriz_prueba, 2)
+        #devuelve la suma del stock por deposito = filas
+        
+        resultado = estimar_stock(totales_por_deposito, depositos, 400, 2)
+        print(resultado)            
+    
+    case '3':
+        totales_por_equipo = calcular_totales(matriz_prueba, 3)
+        #devuelve la suma del stock por equipos = col
+        
+        resultado = estimar_stock(totales_por_equipo, equipos, 500, 3)
+        print(resultado)
+        
+    case '4':
+        result = calcular_maximos(matriz_prueba, depositos, equipos)
+        print(result)
+        
+    case '5':
+        matriz_ventas = [[2,5,7,12,6],
+                         [8,4,11,3,15],
+                         [4,6,9,10,6],
+                         [2,22,5,7,9],
+                         [13,5,7,18,3],
+                         [0,7,19,3,6]]
+        
+        lista_precios = [150,200,100,125,250]
+        pass
+        
---
+++ paquete/modulo.py
@@ -0,0 +1,86 @@
+
+def inicializar_matriz(filas: int, col: int, elemento: any = 0):
+    
+    filas = [[0] * col]
+    
+    for filas in range(len(filas)):    
+        pass
+
+
+def calcular_totales(matriz: list, opcion: int) -> list:
+    
+    if opcion == 2:
+        lista_totales = []
+        acumulador = 0
+    
+        for fila in range(len(matriz)):
+            for col in range(len(matriz[fila])):
+                acumulador += matriz[fila][col] 
+            lista_totales += [acumulador]   
+        
+        return lista_totales
+    
+    elif opcion == 3: 
+        lista_totales_por_col = []
+        acumulador_2 = 0
+        
+        for col in range(len(matriz[0])):
+            for fila in range(len(matriz)):
+                acumulador_2 += matriz[fila][col]
+            lista_totales_por_col += [acumulador_2]
+        
+        return lista_totales_por_col
+
+    else:
+        print('Incorrecto.')
+
+def estimar_stock(lista_totales: list, lista_nombres: list, limite: int, opcion: int)-> None:
+    
+    if opcion == 2:
+        lista_depositos_mas_lim = []
+        
+        for i in range(len(lista_nombres)):
+            if lista_totales[i] > limite:
+                lista_depositos_mas_lim += [lista_nombres[i]]
+
+        print(f'Los depositos con mas de {limite} unidades son: {lista_depositos_mas_lim}')
+        
+    
+    elif opcion == 3:
+        lista_equipos_mas_lim = []
+        
+        for i in range(len(lista_nombres)):
+            if lista_totales[i] > limite:
+                lista_equipos_mas_lim += [lista_nombres[i]]
+
+        print(f'Los equipos con mas de {limite} camisetas son: {lista_equipos_mas_lim}')
+
+
+#punto 4: Obtener máxima cantidad de camisetas de cada equipo. Mostrar en qué depósito se
+#encuentra.
+
+def calcular_maximos(matriz: list, lista_nom: list, lista_nom_2: list)-> None:
+    
+    for i in range(len(lista_nom_2)):
+        flag = False
+        
+        for j in range(len(matriz[i])):    
+            if flag == False:
+                maximo = matriz[j][i]
+                correspondiente = lista_nom[j]
+                flag = True
+                
+            elif matriz[j][i] > maximo:
+                maximo = matriz[j][i]
+                correspondiente = lista_nom[j]        
+        print(f'El {lista_nom_2[i]} tiena la maxima cantidad de camisetas en {correspondiente}, son {maximo}')
+
+
+# Cargar ventas: se deberá poder cargar ventas de un determinado producto para un
+# determinado depósito. Esto es carga distribuida o aleatoria. Al cargarse las ventas
+# se deben restar los productos vendidos del stock y generar una matriz con la
+# recaudación que reciba el listado de precios por parámetro, en caso de no recibir un
+# listado deberá tener un precio de 100 cada producto. Utilizar parámetro opcional.
+ 
+def cargar_ventas(lista_precios: list = 100) -> list:
+    pass
